144f644480a7dbaaf0ee116846d7ac94bd4fce84
onion <kosukekito38@gmail.com>
2024-08-21 12:16:46 +0900

    feat: buttons in header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,16 @@
+import Header from '@/components/Header'
+
 export default function Home() {
-  return <div className="text-green-500">Main Content</div>
+  return (
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcom</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem />
+          </div>
+        </div>
+      </Header>
+    </div>
+  )
 }
